14191775d628db986648b7ae4370dbfa75b4c68a
Alastair Smith <a.smith@iesohealth.com>
2020-11-26 15:43:57 +0000

    Add C# 8 example program

--- csharp-8/Program.cs
+++ csharp-8/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace csharp_8
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public Program() => Console.OutputEncoding = Encoding.UTF8;
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await new Program().RunAsync("https://api.github.com/");
+        }
+
+        public async Task RunAsync([NotNull]string uri)
+        {
+            // static local function
+            static async Task<(Exception? ex, HttpResponseMessage? res)> SendRequest(string url)
+            {
+                // Null-coalescing assignment
+                url ??= "https://api.github.com/";
+
+                Console.WriteLine("Calling GitHub 🐙🐱");
+                try
+                {
+                    var client = new HttpClient();
+                    client.DefaultRequestHeaders.UserAgent.ParseAdd($"Mozilla/5.0 (compatible; github.com/alastairs/csharp-examples)");
+                    var response = await client.GetAsync(url);
+
+                    response.EnsureSuccessStatusCode();
+                    return (null, response);
+                }
+                catch (Exception ex) when (ex is HttpRequestException)
+                {
+                    return (ex, null);
+                }
+            }
+
+            // Improved pattern matching, with switch expressions, for a much more elegant
+            // Either<Error, Result> implementation
+            var response = await SendRequest(uri);
+            switch (response)
+            {
+                case (Exception ex, null):
+                    Console.WriteLine($"The request failed 😞 {ex.Message}");
+                    return;
+                case (null, HttpResponseMessage res):
+                    await WriteResponse(res);
+                    return;
+            }
+        }
+
+        private static async Task WriteResponse(HttpResponseMessage response)
+        {
+            var responseBody = await response.Content.ReadAsStreamAsync();
+            var parsedJson = await JsonSerializer.DeserializeAsync<dynamic>(responseBody);
+
+            // using declarations - calls Dispose() when the variable goes out of scope
+            using var stdout = Console.OpenStandardOutput();
+            await JsonSerializer.SerializeAsync(
+                stdout,
+                parsedJson,
+                new JsonSerializerOptions { WriteIndented = true });
         }
     }
 }
